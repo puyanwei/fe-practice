95e34d5a864b79a9e4ed093229af9db3b4c90427
Puyan Wei <puyanwei@gmail.com>
2021-10-04 10:37:54 +0100

    Setup routing, added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn to use React and be a great developer!
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
