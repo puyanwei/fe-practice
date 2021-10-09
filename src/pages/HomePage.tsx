8679307fa0e230f0c65f2b8ce6db43cd33370454
Puyan Wei <puyanwei@gmail.com>
2021-10-09 21:03:11 +0100

    Add link to Homepage as temp solution

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 const HomePage = () => {
   return (
-    <div>
+    <Layout>
       <h1>Frontend Practice</h1>
-    </div>
+
+      <Link to="/tip-calculator">Tip Calculator</Link>
+    </Layout>
   );
 };
+
+const Layout = styled.div`
+  text-align: center;
+`;
 
 export default HomePage;
