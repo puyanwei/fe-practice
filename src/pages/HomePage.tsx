b67ae87b8d8d78ff2305c33dcab0663adef765e7
Puyan Wei <puyanwei@gmail.com>
2021-10-29 15:46:59 +0100

    Updated homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,34 +4,47 @@ import styled from 'styled-components';
 const HomePage = () => {
   return (
     <Layout>
-      <h1 className="center">Frontend Practice</h1>
-      <p>
+      <h1 className="center title">Frontend Practice</h1>
+      <br />
+      <p className="text-sm">
         Practice website projects mainly from&nbsp;
         <a href="https://www.frontendmentor.io/">Frontend Mentor</a>. All pages
         using React with Typescript
       </p>
-      <Link to="/tip-calculator">Tip Calculator</Link> - from&nbsp;
-      <a href="https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX">
-        this
-      </a>
-      &nbsp;challenge. Uses&nbsp;
-      <a href="https://styled-components.com/">Styled Components</a> for css.
       <br />
-      <Link to="/furniture-shop">Furniture Shop</Link> - from&nbsp;
-      <a href="https://www.frontendmentor.io/challenges/room-homepage-BtdBY_ENq">
-        this
-      </a>
-      &nbsp;challenge. Uses&nbsp;
-      <a href="https://tailwindcss.com/">Tailwind</a>&nbsp;for css
+      <div className="project-list">
+        <Link to="/tip-calculator">Tip Calculator</Link> - from&nbsp;
+        <a href="https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX">
+          this
+        </a>
+        &nbsp;challenge. Uses&nbsp;
+        <a href="https://styled-components.com/">Styled Components</a> for css.
+        <br />
+        <Link to="/furniture-shop">Furniture Shop</Link> - from&nbsp;
+        <a href="https://www.frontendmentor.io/challenges/room-homepage-BtdBY_ENq">
+          this
+        </a>
+        &nbsp;challenge. Uses&nbsp;
+        <a href="https://tailwindcss.com/">Tailwind</a>&nbsp;for css
+      </div>
     </Layout>
   );
 };
 
 const Layout = styled.div`
-  font-family: 'Roboto', sans-serif;
-  max-width: 40rem;
+  max-width: 20rem;
   margin: 1rem auto;
   padding: 0 1rem;
+  font-family: 'Roboto', sans-serif;
+  font-size: small;
+
+  > .title {
+    font-size: xx-large;
+  }
+
+  .project-list {
+    font-size: small;
+  }
 `;
 
 export default HomePage;
